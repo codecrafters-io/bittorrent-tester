351945d881a7c8962a92b431293e604384455df6
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-09-02 22:31:03 +0100

    Fix bencode list decoding and add test for nested list

--- internal/stage_bencode_list.go
+++ internal/stage_bencode_list.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"fmt"
+	"strings"
 
 	tester_utils "github.com/codecrafters-io/tester-utils"
 )
@@ -16,7 +17,13 @@ func testBencodeList(stageHarness *tester_utils.StageHarness) error {
 	randomWordEncoded := fmt.Sprintf("%d:%s", len(randomWord), randomWord)
 	listEncoded := fmt.Sprintf("l%si52ee", randomWordEncoded)
 
+	list := []string{
+		fmt.Sprintf("[\"%s\",52]\n", randomWord),
+		fmt.Sprintf("[\"%s\", 52]\n", randomWord),
+	}
+
 	logger.Infof("Running ./your_bittorrent.sh decode %s", listEncoded)
+	logger.Infof("Expected output: %s", strings.TrimSpace(list[0]))
 	result, err := executable.Run("decode", listEncoded)
 	if err != nil {
 		return err
@@ -26,11 +33,30 @@ func testBencodeList(stageHarness *tester_utils.StageHarness) error {
 		return err
 	}
 
-	list := []string{
-		fmt.Sprintf("[\"%s\",52]\n", randomWord),
-		fmt.Sprintf("[\"%s\", 52]\n", randomWord),
-	}
 	if err = assertStdoutList(result, list); err != nil {
+		return err
+	}
+
+	// Test for a nested list
+	nestedListEncoded := fmt.Sprintf("ll%si52eee", randomWordEncoded)
+
+	nestedList := []string{
+		fmt.Sprintf("[[\"%s\",52]]\n", randomWord),
+		fmt.Sprintf("[[\"%s\", 52]]\n", randomWord),
+	}
+
+	logger.Infof("Running ./your_bittorrent.sh decode %s", nestedListEncoded)
+	logger.Infof("Expected output: %s", strings.TrimSpace(nestedList[0]))
+	result, err = executable.Run("decode", nestedListEncoded)
+	if err != nil {
+		return err
+	}
+
+	if err = assertExitCode(result, 0); err != nil {
+		return err
+	}
+
+	if err = assertStdoutList(result, nestedList); err != nil {
 		return err
 	}
 
