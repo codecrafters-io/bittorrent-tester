328c0a55f2cce7be5710fae0cab904c89ea791f3
Sarp Centel <sarpcentel@gmail.com>
2023-10-25 00:25:13 +0300

    add extra test for stage 1

--- internal/stage_bencode_string.go
+++ internal/stage_bencode_string.go
@@ -30,5 +30,23 @@ func testBencodeString(stageHarness *tester_utils.StageHarness) error {
 		return err
 	}
 
+	trackerURL := "http://bittorrent-test-tracker.codecrafters.io/announce"
+	trackerURLEncoded := fmt.Sprintf("%d:%s", len(trackerURL), trackerURL)
+
+	logger.Infof("Running ./your_bittorrent.sh decode %s", trackerURLEncoded)
+	result, err = executable.Run("decode", trackerURLEncoded)
+	if err != nil {
+		return err
+	}
+
+	if err = assertExitCode(result, 0); err != nil {
+		return err
+	}
+
+	expected = fmt.Sprintf("\"%s\"\n", trackerURL)
+	if err = assertStdout(result, expected); err != nil {
+		return err
+	}
+
 	return nil
 }
