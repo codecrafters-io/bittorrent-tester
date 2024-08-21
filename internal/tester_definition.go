7887e8b522e8d33c11fe0bbea5b6bcf3e9b80a8d
Sarp Centel <sarpcentel@gmail.com>
2024-08-21 23:49:20 +0300

    add tests to tester definition

--- internal/tester_definition.go
+++ internal/tester_definition.go
@@ -56,5 +56,38 @@ var testerDefinition = tester_definition.TesterDefinition{
 			TestFunc: testDownloadFile,
 			Timeout:  90 * time.Second,
 		},
+		{
+			Slug: "hw0",
+			TestFunc: testParseMagnetLink,
+		},
+		{
+			Slug: "pk2",
+			TestFunc: testMagnetReserved,
+		},
+		{
+			Slug: "xi4",
+			TestFunc: testMagnetSendExtendedHandshake,
+		},
+		{
+			Slug: "jk6",
+			TestFunc: testMagnetReceiveExtendedHandshake,
+		},
+		{
+			Slug: "ns5",
+			TestFunc: testMagnetRequestMetadata,
+		},
+		{
+			Slug: "zh1",
+			TestFunc: testMagnetSendMetadata,
+		},
+		{
+			Slug: "qv6",
+			TestFunc: testMagnetDownloadPiece,
+		},
+		{
+			Slug: "dv7",
+			TestFunc: testMagnetDownloadFile,
+			Timeout:  90 * time.Second,
+		},
 	},
 }
