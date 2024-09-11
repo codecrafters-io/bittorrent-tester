bb4ba7affa9db17a19ec62387126d4e2a0022dd8
Sarp Centel <sarpcentel@gmail.com>
2024-09-11 20:35:13 +0300

    change

--- internal/stages_test.go
+++ internal/stages_test.go
@@ -34,7 +34,7 @@ func TestStages(t *testing.T) {
 			NormalizeOutputFunc: normalizeTesterOutput,
 		},
 		"pass_all": {
-			UntilStageSlug:      "zh1",
+			UntilStageSlug:      "dv7",
 			CodePath:            "./test_helpers/scenarios/pass_all",
 			ExpectedExitCode:    0,
 			StdoutFixturePath:   "./test_helpers/fixtures/pass_all",
