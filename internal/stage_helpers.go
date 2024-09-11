96f12445f41474689d44b670cb2acad7af64de4a
Sarp Centel <sarpcentel@gmail.com>
2024-09-11 19:18:32 +0300

    test

--- internal/stage_helpers.go
+++ internal/stage_helpers.go
@@ -14,6 +14,7 @@ import (
 	"path"
 	"strconv"
 	"strings"
+	"time"
 
 	logger "github.com/codecrafters-io/tester-utils/logger"
 	"github.com/jackpal/bencode-go"
@@ -443,6 +444,7 @@ func receiveAndSendHandshake(conn net.Conn, peer PeerConnectionParams) (err erro
 
 func waitAndHandlePeerConnection(p PeerConnectionParams, handler ConnectionHandler) {
 	logger := p.logger
+	time.Sleep(1 * time.Millisecond) // for deterministic output ordering in fixtures
 	logger.Debugf("Peer listening on address: %s", p.address)
 	listener, err := net.Listen("tcp", p.address)
 	if err != nil {
